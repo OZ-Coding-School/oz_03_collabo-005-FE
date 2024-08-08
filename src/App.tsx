74b75a8817be8c35772094e2fedff284165b5692
KingBoRam <bory2321@naver.com>
2024-08-08 17:27:44 +0900

    ✨ Feat: button 기능 개발
    버튼 타이틀 수정함

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
       <h1 className="mt-2 text-3xl font-bold">Vite + React</h1>
       <div className="mt-4 space-x-4">
         <button className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700">1번버튼</button>
-        <button className="rounded bg-red-500 px-4 py-2 font-bold text-white hover:bg-red-700">2번버튼</button>
+        <button className="rounded bg-red-500 px-4 py-2 font-bold text-white hover:bg-red-700">2번버튼이요</button>
       </div>
       <p className="read-the-docs mt-4">Click on the Vite and React logos to learn more</p>
     </div>
