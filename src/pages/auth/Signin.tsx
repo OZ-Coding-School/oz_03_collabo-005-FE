80723294c0e064ec4a4a79b4a0b9f2e35a9eadb6
junhyeon0218 <junhyeon0218@daum.net>
2024-08-13 17:02:01 +0900

    ✨ 로그인, 회원가입, 비밀번호 변경 페이지 제작

--- src/pages/auth/Signin.tsx
+++ src/pages/auth/Signin.tsx
@@ -1,5 +1,77 @@
+import { useForm, SubmitHandler } from 'react-hook-form';
+import Input from '../../components/common/Input';
+import Button from '../../components/common/Button';
+import { Link } from 'react-router-dom';
+
+interface InputProps {
+  email: string;
+  password: string;
+}
+
 const Signin = () => {
-  return <div>Signin</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting, errors },
+  } = useForm<InputProps>();
+
+  const onSubmit: SubmitHandler<InputProps> = (data) => console.log(data);
+
+  return (
+    <div className="px-[16px] pt-[12px]">
+      <form noValidate onSubmit={handleSubmit(onSubmit)}>
+        <Input
+          title="이메일"
+          name="email"
+          placeholder="이메일을 입력하세요."
+          inputSize="normal"
+          type="email"
+          register={register('email', {
+            required: '이메일을 입력하세요.',
+            pattern: {
+              value: /\S+@\S+\.\S+/,
+              message: '이메일 양식이 올바르지 않습니다.',
+            },
+          })}
+          error={errors.email}
+        />
+
+        <Input
+          title="비밀번호"
+          name="password"
+          placeholder="비밀번호를 입력하세요."
+          inputSize="normal"
+          type="password"
+          register={register('password', {
+            required: '비밀번호를 입력하세요.',
+            minLength: {
+              value: 6,
+              message: '비밀번호는 6글자 이상',
+            },
+          })}
+          error={errors.password}
+        />
+
+        <Button
+          buttonSize="normal"
+          bgColor="filled"
+          className="mb-[24px] h-[48px] font-bold"
+          disabled={isSubmitting}
+          type="submit">
+          로그인
+        </Button>
+        <Button buttonSize="normal" bgColor="ghost" className="h-[48px] font-bold">
+          <Link to={'/signup'}>이메일로 간편 가입</Link>
+        </Button>
+      </form>
+      <p className="mt-[16px] text-center text-[12px] text-gray-98">
+        로그인(가입) 시 이용약관 및 개인정보 처리방침에 동의하는 것으로 간주합니다.
+      </p>
+      <p className="mt-[38px] text-[14px] font-bold text-[#999999]">
+        <Link to={'/signin/resetpassword'}>비밀번호를 잊어버리셨나요?</Link>
+      </p>
+    </div>
+  );
 };
 
 export default Signin;
--- src/pages/auth/Signup.tsx
+++ src/pages/auth/Signup.tsx
@@ -1,5 +1,96 @@
+import { useForm, SubmitHandler } from 'react-hook-form';
+import Input from '../../components/common/Input';
+import Button from '../../components/common/Button';
+
+interface InputProps {
+  email: string;
+  password: string;
+  confirmPassword: string;
+  nickname: string;
+}
+
 const Signup = () => {
-  return <div>Signup</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting, errors },
+    getValues,
+  } = useForm<InputProps>();
+
+  const onSubmit: SubmitHandler<InputProps> = (data) => console.log(data);
+
+  return (
+    <form noValidate onSubmit={handleSubmit(onSubmit)} className="px-[16px] pt-[12px]">
+      <Input
+        title="이메일"
+        name="email"
+        placeholder="이메일을 입력하세요."
+        inputSize="normal"
+        type="email"
+        register={register('email', {
+          required: '이메일을 입력하세요.',
+          pattern: {
+            value: /\S+@\S+\.\S+/,
+            message: '이메일 양식이 올바르지 않습니다.',
+          },
+        })}
+        error={errors.email}
+      />
+
+      <Input
+        title="비밀번호"
+        name="password"
+        placeholder="비밀번호를 입력하세요."
+        inputSize="normal"
+        type="password"
+        register={register('password', {
+          required: '비밀번호를 입력하세요.',
+          minLength: {
+            value: 6,
+            message: '비밀번호는 6글자 이상이어야 합니다.',
+          },
+        })}
+        error={errors.password}
+      />
+
+      <Input
+        title="비밀번호 확인"
+        name="confirmPassword"
+        placeholder="비밀번호를 한번 더 입력하세요."
+        inputSize="normal"
+        type="password"
+        register={register('confirmPassword', {
+          required: '비밀번호를 입력하세요.',
+          validate: (value) => value === getValues('password') || '비밀번호가 일치하지 않습니다.',
+        })}
+        error={errors.confirmPassword}
+      />
+
+      <Input
+        title="닉네임"
+        name="nickname"
+        placeholder="닉네임을 입력하세요."
+        inputSize="normal"
+        type="text"
+        register={register('nickname', {
+          required: '닉네임임을 입력하세요.',
+          minLength: {
+            value: 2,
+            message: '닉네임은 2자 이상이어야 합니다.',
+          },
+          maxLength: {
+            value: 10,
+            message: '닉네임은 10자 이하로 입력하세요.',
+          },
+        })}
+        error={errors.nickname}
+      />
+
+      <Button buttonSize="normal" bgColor="filled" className="h-[48px] font-bold" disabled={isSubmitting} type="submit">
+        가입완료
+      </Button>
+    </form>
+  );
 };
 
 export default Signup;
