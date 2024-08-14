38eefbb841b723f5085ab525577f8c698d463d13
jinyeongjang <wlsdud4222@gmail.com>
2024-08-14 16:37:05 +0900

    ✨ Feat: HeaderLanding 기능 추가

---
+++ src/components/layout/HeaderLanding.tsx
@@ -0,0 +1,9 @@
+const HeaderLanding = () => {
+  return (
+    <div className="fixed z-50 mt-0 flex h-[72px] w-full items-center justify-center bg-white px-2 py-5 text-xl font-semibold">
+      <div className="ml-[265px] w-full flex-1">밥피엔스</div>
+    </div>
+  );
+};
+
+export default HeaderLanding;
