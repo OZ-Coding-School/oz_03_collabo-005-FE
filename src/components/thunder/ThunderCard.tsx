d82e523f5f264eae132bb0593a1b426b0c2d6d3a
KingBoRam <bory2321@naver.com>
2024-08-20 18:55:50 +0900

    ✨ Feat: 이미지 로딩 전 스켈레톤 추가

--- src/components/thunder/ThunderCard.tsx
+++ src/components/thunder/ThunderCard.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { format, isToday, isThisWeek, addWeeks, startOfWeek, endOfWeek, StartOfWeekOptions } from 'date-fns';
 import { ko } from 'date-fns/locale';
+import Badge from './Badge';
 
 interface ThunderCardProps {
   id: number;
@@ -24,21 +25,18 @@ const ThunderCard: React.FC<ThunderCardProps> = ({
   genderGroup,
   ageGroup,
 }) => {
+  const [imageLoaded, setImageLoaded] = useState(false);
   const defaultImageUrl = '/images/CuteEgg.svg';
 
   const isNextWeek = (date: Date, options: StartOfWeekOptions = { weekStartsOn: 1 }): boolean => {
-    // 다음 주의 시작
     const nextWeekStart = startOfWeek(addWeeks(new Date(), 1), options);
-    // 다음 주의 끝
     const nextWeekEnd = endOfWeek(addWeeks(new Date(), 1), options);
-    // date가 다음주의 시작~끝 사이에 있으면 다음주에 포함됨으로 true 반환
     return date >= nextWeekStart && date <= nextWeekEnd;
   };
 
   const formatAppointmentTime = (appointmentTime: string): string => {
     const date = new Date(appointmentTime);
     if (isToday(date)) {
-      // a: 오전오후, eeee: 요일(한글로 표기하기위해 locale:ko, h: 시간, mm: 분
       return `오늘 ${format(date, 'a h:mm', { locale: ko })}`;
     } else if (isThisWeek(date, { weekStartsOn: 1 })) {
       return `이번 주 ${format(date, 'eeee, a h:mm', { locale: ko })}`;
@@ -55,10 +53,16 @@ const ThunderCard: React.FC<ThunderCardProps> = ({
     <Link to={`/thunder/${id}`} className="flex w-full flex-col">
       <div className="my-2 flex h-full items-center">
         <div className="flex-shrink-0">
+          {!imageLoaded && (
+            <div className="block h-[140px] w-[140px] rounded-[10px] bg-gray-300 xs:h-[120px] xs:w-[120px]" />
+          )}
           <img
-            className="block h-[140px] w-[140px] rounded-[8px] object-cover xs:h-[120px] xs:w-[120px]"
+            className={`block h-[140px] w-[140px] rounded-[10px] object-cover xs:h-[120px] xs:w-[120px] ${
+              imageLoaded ? 'block' : 'hidden'
+            }`}
             src={imageUrl || defaultImageUrl}
             alt={title}
+            onLoad={() => setImageLoaded(true)}
           />
         </div>
         <div className="ml-4 flex h-full grow flex-col justify-between gap-1">
@@ -78,13 +82,3 @@ const ThunderCard: React.FC<ThunderCardProps> = ({
 };
 
 export default ThunderCard;
-
-interface BadgeProps {
-  label: string;
-}
-
-const Badge: React.FC<BadgeProps> = ({ label }) => (
-  <span className="flex items-center justify-center rounded border border-solid bg-black bg-opacity-5 px-[6px] py-[1px] text-[14px] xs:px-[4px] xs:py-[2px] xs:text-[12px]">
-    {label}
-  </span>
-);
