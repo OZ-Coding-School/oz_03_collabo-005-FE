85bf93f504892b8f085e315b2ed0696fbf5f19ba
KingBoRam <bory2321@naver.com>
2024-08-26 10:23:23 +0900

    🎨 Design: header 반응형 수정

--- src/components/layout/HeaderLanding.tsx
+++ src/components/layout/HeaderLanding.tsx
@@ -1,7 +1,7 @@
 const HeaderLanding = () => {
   return (
-    <div className="fixed z-50 mt-0 flex h-[72px] w-full max-w-[600px] items-center justify-center bg-white px-2 py-5 text-xl font-semibold">
-      <div className="ml-[265px] w-full max-w-[600px] flex-1 xs:right-[400px] xs:ml-[150px]">밥피엔스</div>
+    <div className="fixed z-50 mt-0 flex h-[72px] w-full max-w-[600px] items-center justify-center bg-white px-2 py-5 text-xl font-semibold xs:h-[52px]">
+      <div className="flex w-full max-w-[600px] items-center justify-center">밥피엔스</div>
     </div>
   );
 };
