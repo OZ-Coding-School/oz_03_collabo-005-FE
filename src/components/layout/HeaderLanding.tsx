061b990302fc7fcd683fba50e0f83e9d2e4b8d55
jinyeongjang <wlsdud4222@gmail.com>
2024-08-30 17:54:08 +0900

    🎨 Design: 밥피엔스 로고 추가

--- src/components/layout/HeaderLanding.tsx
+++ src/components/layout/HeaderLanding.tsx
@@ -1,7 +1,9 @@
 const HeaderLanding = () => {
   return (
     <div className="fixed z-50 mt-0 flex h-[72px] w-full max-w-[600px] items-center justify-center bg-white px-2 py-5 text-xl font-semibold xs:h-[52px]">
-      <div className="flex w-full max-w-[600px] items-center justify-center">밥피엔스</div>
+      <div className="flex w-full max-w-[600px] items-center justify-center">
+        <img src="/images/bobpience_logo2.png" alt="Logo" className="h-full w-[150px]" />
+      </div>
     </div>
   );
 };
