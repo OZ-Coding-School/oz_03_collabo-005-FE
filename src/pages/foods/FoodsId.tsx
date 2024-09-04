4abe1ecfd5aab8f97cc22a12ede7a98adb09ae99
junhyeon0218 <junhyeon0218@daum.net>
2024-09-04 23:31:59 +0900

    ✨ Feat: 더미데이터에서 실제 데이터 사용변경

--- src/pages/foods/FoodsId.tsx
+++ src/pages/foods/FoodsId.tsx
@@ -1,12 +1,13 @@
 import { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import FOOD_DATA from '../../DUMMY_DATA/FOOD_DATA.json';
-import { useFoodStore } from '../../store/foodStore/foodStore';
+import { useLocation } from 'react-router-dom';
+import { useFoodStore } from '../../store/foodStore';
 import Map from '../../components/foods/KakaoMap';
 
 const FoodsId = () => {
-  const { foodsId } = useParams<string>();
-  const { setFoodName, searchResults, selectedRestaurant, setSelectedRestaurant } = useFoodStore();
+  const { setFoodName, searchResults, selectedRestaurant, setSelectedRestaurant, foodsList } = useFoodStore();
+  const location = useLocation();
+  const { name } = location.state || {};
+  console.log(name);
 
   const handleRestaurantClick = (id: string) => {
     setSelectedRestaurant(id);
@@ -14,16 +15,13 @@ const FoodsId = () => {
   };
 
   useEffect(() => {
-    const foodData = FOOD_DATA.find((item) => item.id === foodsId);
-    if (foodData) {
-      setFoodName(foodData.name);
-    }
-  }, [foodsId, setFoodName]);
+    setFoodName(name);
+  }, [setFoodName, foodsList]);
 
   return (
-    <div className="relative flex h-[100%] flex-col overflow-y-hidden">
-      <Map className="z-10" />
-      <div className="absolute bottom-0 z-20 flex max-h-[42%] min-h-[134px] w-full flex-col rounded-t-[16px] bg-white shadow-[0_-2px_21px_0_rgba(0,0,0,0.25)]">
+    <div className="relative flex h-[calc(100vh-72px)] flex-col overflow-y-hidden xs:h-[calc(100vh-52px)]">
+      <Map className="z-10 grow" />
+      <div className="z-20 flex max-h-[40%] min-h-[134px] w-full flex-col rounded-t-[16px] bg-white shadow-[0_-2px_21px_0_rgba(0,0,0,0.25)]">
         <div className="mx-auto mb-[20px] mt-[12px] h-[6px] min-h-[6px] w-[66px] rounded-full bg-gray-d9" />
 
         <div className="flex flex-col overflow-y-scroll scrollbar-hide">
