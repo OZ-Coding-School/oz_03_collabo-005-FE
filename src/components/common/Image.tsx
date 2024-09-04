dcdb3b38ed816d2d67ca2fb882a53cab86791640
KingBoRam <bory2321@naver.com>
2024-09-04 15:10:54 +0900

    🐛 Fix: 이미지 경로가 아닌 경우 대체 이미지로 노출

--- src/components/common/Image.tsx
+++ src/components/common/Image.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 interface ImageProps extends Omit<React.ImgHTMLAttributes<HTMLImageElement>, 'src' | 'alt'> {
@@ -7,13 +7,28 @@ interface ImageProps extends Omit<React.ImgHTMLAttributes<HTMLImageElement>, 'sr
 }
 
 const Image: React.FC<ImageProps> = (props) => {
+  const [imgSrc, setImgSrc] = useState(props.src); // 이미지 소스를 상태로 관리
   const navigate = useNavigate();
 
   const handleClick = () => {
-    navigate('/image', { state: { src: props.src, alt: props.alt } });
+    navigate('/image', { state: { src: imgSrc, alt: props.alt } });
   };
 
-  return <img {...props} onClick={handleClick} className={`cursor-pointer ${props.className || ''}`} />;
+  const handleError = () => {
+    // 이미지 로드 실패 시 기본 이미지로 변경
+    setImgSrc('/images/anonymous_avatars.svg');
+  };
+
+  return (
+    <img
+      {...props}
+      src={imgSrc}
+      onClick={handleClick}
+      onError={handleError} // 이미지 로드 실패 시 handleError 호출
+      className={`cursor-pointer ${props.className || ''}`}
+      alt={props.alt}
+    />
+  );
 };
 
 export default Image;
