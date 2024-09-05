55e31f4bc11b527e2e6f103b87e9c2f38a3689fc
KingBoRam <bory2321@naver.com>
2024-09-05 18:57:24 +0900

    ✨ Feat: fti 테스트 저장하는 api 적용

--- src/components/myprofile/UserInfo.tsx
+++ src/components/myprofile/UserInfo.tsx
@@ -1,28 +1,55 @@
-import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { authInstance } from '../../api/util/instance';
+import ModalCenter from '../common/ModalCenter';
 
 interface User {
-  fti_type_name: string | null;
-  introduce: string | null;
   nickname: string;
-  profile_image_url: string | null;
+  introduce: string | null;
+  fti_type_name: string | null;
   spicy_preference: number | null;
+  profile_image_url: string | null;
 }
 
 interface UserInfoProps {
   user?: User;
+  refetchUserProfile: () => void;
 }
 
-const UserInfo: React.FC<UserInfoProps> = ({ user }) => {
+const UserInfo: React.FC<UserInfoProps> = ({ user, refetchUserProfile }) => {
   const location = useLocation();
+  const navigate = useNavigate();
   const isProfilePage = location.pathname.includes('/profile/');
+
+  const [isModalCenterOpen, setIsModalCenterOpen] = useState(false);
+  const openModalCenter = () => setIsModalCenterOpen(true);
+  const closeModalCenter = () => setIsModalCenterOpen(false);
 
   const handleImageError = (event: React.SyntheticEvent<HTMLImageElement, Event>) => {
     event.currentTarget.src = '/images/anonymous_avatars.svg';
   };
 
+  const handleConfirmFti = () => {
+    const FtiResults = localStorage.getItem('FtiResults');
+    if (FtiResults) {
+      openModalCenter();
+    } else {
+      navigate('/FTI');
+    }
+  };
+
+  const handleSaveResults = () => {
+    const FtiResults = { fti_style: [localStorage.getItem('FtiResults')] };
+    if (FtiResults) {
+      authInstance.post('/api/ftitests/result/', FtiResults).then(() => {
+        refetchUserProfile();
+        closeModalCenter();
+      });
+    }
+  };
+
   return (
-    <div className="flex h-[35%] flex-col items-center justify-evenly p-[1rem]">
+    <div className="m-[1rem] flex h-[300px] flex-col items-center justify-evenly xs:m-2 xs:h-[200px]">
       <div className="relative flex w-[30%] items-center justify-center pt-[30%] xs:w-[35%] xs:pt-[35%]">
         <Link to={user ? `/profile/${user.nickname}` : '#'}>
           <img
@@ -33,7 +60,6 @@ const UserInfo: React.FC<UserInfoProps> = ({ user }) => {
           />
         </Link>
       </div>
-
       {!user ? (
         <Link to="/signin" className="flex h-[8rem] flex-col items-center justify-center">
           <p className="text-[22px] font-bold xs:text-[18px]">로그인이 필요합니다</p>
@@ -46,12 +72,27 @@ const UserInfo: React.FC<UserInfoProps> = ({ user }) => {
             (user.fti_type_name ? (
               <p className="text-[16px] text-[#AD5E33] xs:text-[12px]">{user.fti_type_name}</p>
             ) : (
-              <Link to="/FTI" className="text-[16px] text-[#AD5E33] xs:text-[12px]">
+              <div onClick={handleConfirmFti} className="cursor-pointer text-[16px] text-[#AD5E33] xs:text-[12px]">
                 테스트를 통해 FTI타입을 확인해보세요
-              </Link>
+              </div>
             ))}
         </div>
       )}
+      <ModalCenter isOpen={isModalCenterOpen} title1="저장된 FTI 검사 결과가 있습니다." onClose={closeModalCenter}>
+        <p className="text-[#666666] xs:text-[14px]">이전 테스트 결과를 프로필에 저장하시겠어요?</p>
+        <div className="mt-8 flex w-full gap-3">
+          <button
+            onClick={closeModalCenter}
+            className="w-full rounded-xl border-2 border-orange-400 px-1 py-2 font-semibold text-orange-500 hover:bg-orange-50">
+            FTI 테스트로 이동
+          </button>
+          <button
+            onClick={handleSaveResults}
+            className="w-full rounded-xl border-2 border-orange-500 bg-orange-500 px-1 py-2 font-semibold text-white hover:border-orange-600 hover:bg-orange-600">
+            결과 저장
+          </button>
+        </div>
+      </ModalCenter>
     </div>
   );
 };
--- src/pages/myprofile/MyProfile.tsx
+++ src/pages/myprofile/MyProfile.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import { useEffect, useState } from 'react';
 import { IoIosArrowForward } from 'react-icons/io';
@@ -8,15 +8,17 @@ import UserLink from '../../components/myprofile/UserLink';
 import ModalBottom from '../../components/common/ModalBottom';
 import ModalCenter from '../../components/common/ModalCenter';
 import { authInstance, logout } from '../../api/util/instance';
+import { getCookie } from '../../utils/cookie';
 
 const MyProfile = () => {
   const [user, setUser] = useState(undefined);
   const [isLoading, setIsLoading] = useState(true);
   const [isModalCenterOpen, setIsModalCenterOpen] = useState(false);
   const [isLogoutConfirmOpen, setIsLogoutConfirmOpen] = useState(false);
+  const navigate = useNavigate();
 
-  useEffect(() => {
-    const fetchUserProfile = async () => {
+  const fetchUserProfile = async () => {
+    if (getCookie('refresh')) {
       try {
         const res = await authInstance.get('/api/profile');
         setUser(res.data);
@@ -25,7 +27,12 @@ const MyProfile = () => {
       } finally {
         setIsLoading(false);
       }
-    };
+    } else {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
     fetchUserProfile();
   }, []);
 
@@ -62,19 +69,19 @@ const MyProfile = () => {
 
   return (
     <>
-      <UserInfo user={user} />
-      <div className="flex items-center justify-center p-[16px]">
+      <UserInfo user={user} refetchUserProfile={fetchUserProfile} />
+      <div className="flex items-center justify-center p-[16px] xs:p-2">
         <Link
           to={user ? '/flavor' : '#'}
           onClick={handlePreventNavigate}
-          className="mr-2 flex h-[62px] w-[43%] items-center justify-center rounded-lg bg-primary text-[20px] font-bold text-white xs:h-[42px] xs:text-[16px]">
+          className="mr-2 flex h-[52px] w-[43%] items-center justify-center rounded-lg bg-primary text-[20px] font-bold text-white xs:h-[42px] xs:text-[14px]">
           내 입맛 설정
         </Link>
         <Link
           to={user ? '/myprofile/myprofileedit' : '#'}
           state={{ user }}
           onClick={handlePreventNavigate}
-          className="ml-2 flex h-[62px] w-[43%] items-center justify-center rounded-lg bg-[#F5E3DB] text-[20px] font-bold xs:h-[42px] xs:text-[16px]">
+          className="ml-2 flex h-[52px] w-[43%] items-center justify-center rounded-lg bg-[#F5E3DB] text-[20px] font-bold xs:h-[42px] xs:text-[14px]">
           내 프로필
         </Link>
       </div>
@@ -83,7 +90,7 @@ const MyProfile = () => {
       <UserLink text={'나의 맛있는 발견'} src={'/myprofile/myprofileboard'} isUserLoggedIn={!!user} />
       <Link to={'https://suist.notion.site/e2c6e050f097489fb620469d397f70d8'} target="_blank">
         <motion.div whileHover={{ scale: 1.03 }} transition={{ type: 'spring', stiffness: 200 }}>
-          <div className="flex w-full cursor-pointer items-center justify-between p-[12px] text-[20px] hover:rounded-lg hover:font-bold hover:text-primary xs:py-[0.7rem] xs:text-[14px]">
+          <div className="flex w-full cursor-pointer items-center justify-between p-[12px] px-4 text-[20px] hover:rounded-lg hover:font-bold hover:text-primary xs:py-[0.7rem] xs:text-[14px]">
             이용약관
             <IoIosArrowForward className="text-[20px] xs:text-[16px]" />
           </div>
@@ -94,13 +101,13 @@ const MyProfile = () => {
           onClick={() => setIsLogoutConfirmOpen(true)}
           whileHover={{ scale: 1.03 }}
           transition={{ type: 'spring', stiffness: 200 }}>
-          <div className="flex w-full cursor-pointer items-center justify-between p-[12px] text-[20px] hover:rounded-lg hover:font-bold hover:text-primary xs:py-[0.7rem] xs:text-[14px]">
+          <div className="flex w-full cursor-pointer items-center justify-between p-[12px] px-4 text-[20px] hover:rounded-lg hover:font-bold hover:text-primary xs:py-[0.7rem] xs:text-[14px]">
             로그아웃
             <IoIosArrowForward className="text-[20px] xs:text-[16px]" />
           </div>
         </motion.div>
       )}
-      <div className="flex items-center justify-center p-[16px]">
+      <div className="flex items-center justify-center p-[16px] xs:p-2">
         <button
           onClick={() => openPopup('https://pf.kakao.com/_xixaBxoG/chat')}
           className="ml-2 flex h-[6vh] w-[43%] items-center justify-center text-[18px] font-bold xs:text-[14px]">
@@ -117,31 +124,35 @@ const MyProfile = () => {
         title1="로그인이 필요한 서비스 입니다."
         title2=""
         onClose={() => setIsModalCenterOpen(false)}>
-        <div className="mt-8 flex w-full space-x-4">
+        <p className="text-[#666666] xs:text-[14px]">로그인 페이지로 이동하시겠어요?</p>
+        <div className="mt-8 flex w-full justify-evenly gap-4">
           <button
             onClick={() => setIsModalCenterOpen(false)}
-            className="w-full flex-1 rounded-xl border-2 border-orange-400 px-1 py-2 font-semibold text-orange-500 hover:bg-orange-50">
+            className="w-full rounded-xl border-2 border-orange-400 px-1 py-2 font-semibold text-orange-500 hover:bg-orange-50">
             취소
           </button>
-          <Link to="/signin" className="flex-1">
-            <button className="w-full rounded-xl bg-orange-500 px-2 py-3 font-semibold text-white hover:bg-orange-600">
-              로그인
-            </button>
-          </Link>
+          <button
+            onClick={() => {
+              navigate('/signin');
+            }}
+            className="w-full rounded-xl border-2 border-orange-500 bg-orange-500 px-1 py-2 font-semibold text-white hover:border-orange-600 hover:bg-orange-600">
+            로그인
+          </button>
         </div>
       </ModalCenter>
       <ModalBottom isOpen={isLogoutConfirmOpen} onClose={() => setIsLogoutConfirmOpen(false)}>
         <div className="mx-auto h-[6px] w-[66px] rounded-[8px] bg-[#d9d9d9]" />
-        <p className="mt-8 text-center text-[24px] font-bold">로그아웃 하시겠습니까?</p>
-        <div className="mb-4 mt-8 flex w-full space-x-4">
+        <p className="my-8 text-center text-[24px] font-bold">로그아웃 하시겠습니까?</p>
+        <p className="text-center text-[#666666] xs:text-[14px]">로그인 상태를 유지하고 싶다면, 취소를 눌러주세요.</p>
+        <div className="mb-4 mt-8 flex w-full gap-4">
           <button
             onClick={() => setIsLogoutConfirmOpen(false)}
-            className="w-full flex-1 rounded-xl border-2 border-orange-400 px-1 py-2 font-semibold text-orange-500 hover:bg-orange-50">
+            className="w-full rounded-xl border-2 border-orange-400 px-1 py-2 font-semibold text-orange-500 hover:bg-orange-50">
             취소
           </button>
           <button
             onClick={handleLogout}
-            className="w-full flex-1 rounded-xl bg-orange-500 px-2 py-3 font-semibold text-white hover:bg-orange-600">
+            className="w-full rounded-xl border-2 border-orange-500 bg-orange-500 px-1 py-2 font-semibold text-white hover:border-orange-600 hover:bg-orange-600">
             로그아웃
           </button>
         </div>
