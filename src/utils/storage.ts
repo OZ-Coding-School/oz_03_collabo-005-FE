a5033e89660d3a4674fc30a5b5cfacfd5bb16464
junhyeon0218 <junhyeon0218@daum.net>
2024-09-08 04:20:02 +0900

    ✨ Feat: 로컬스토리지 코드 추가

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -9,3 +9,15 @@ export const setItem = (item: string, value: string): void => {
 export const removeItem = (item: string): void => {
   return sessionStorage.removeItem(item);
 };
+
+export const localgetItem = (item: string): string | null => {
+  return localStorage.getItem(item);
+};
+
+export const localsetItem = (item: string, value: string): void => {
+  return localStorage.setItem(item, value);
+};
+
+export const localremoveItem = (item: string): void => {
+  return localStorage.removeItem(item);
+};
