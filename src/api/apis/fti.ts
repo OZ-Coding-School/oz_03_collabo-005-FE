9e7917b64a557bf1788b52f1da56378f0e0fa9c6
junhyeon0218 <junhyeon0218@daum.net>
2024-09-08 04:16:49 +0900

    ✨ Feat: fti결과 보내기(유저정보 보내기)

--- src/api/apis/fti.ts
+++ src/api/apis/fti.ts
@@ -1,4 +1,5 @@
-import { baseInstance } from '../util/instance';
+import { baseInstance, authInstance } from '../util/instance';
+import { getCookie } from '../../utils/cookie';
 
 export const getFtiQuestions = async () => {
   try {
@@ -12,14 +13,16 @@ export const getFtiQuestions = async () => {
 
 export const sendFtiResult = async (result: string[]) => {
   try {
-    const response = await baseInstance.post('/api/ftitests/result/', { fti_style: result }); // 'results' 키로 전송
+    const refreshToken = getCookie('refresh'); // 쿠키에서 리프레시 토큰 가져오기
+    const instance = refreshToken ? authInstance : baseInstance; // 리프레시 토큰이 있으면 authInstance 사용, 없으면 baseInstance 사용
+
+    const response = await instance.post('/api/ftitests/result/', { fti_style: result });
     return response.data;
   } catch (error) {
     console.error('Failed to send FTI result:', error);
     throw error;
   }
 };
-
 export const getFtiType = async (uuid: string) => {
   try {
     const response = await baseInstance.get(`/api/ftitests/result/${uuid}`);
