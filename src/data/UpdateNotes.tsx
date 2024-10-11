103e9282aad842d3adb1498e5f7f59822e8c576d
jinyeongjang <wlsdud4222@gmail.com>
2024-10-11 21:27:35 +0900

    ✨ Feat: 업데이트 노트 페이지 기능 추가

---
+++ src/data/UpdateNotes.tsx
@@ -0,0 +1,50 @@
+export const UpdateNotes = async () => {
+  return [
+    {
+      uuid: '1',
+      category_name: '개선사항',
+      title: 'v1.0.1 사용자 경험 개선',
+      content: '',
+      description:
+        '크롬, 사파리 등에서 모바일 환경에서 스크롤을 두번 더 내려야 아래 버튼이 나오는 현상이 수정되었습니다. 사용자 경험 개선을 위한 업데이트 입니다. 로그인을 하지 않았을 경우 좋아요 버튼을 누르거나, 댓글을 작성할 때 로그인 안내 모달이 새롭게 추가되었습니다',
+      created_at: '2024-09-20',
+      image_url: '',
+    },
+    {
+      uuid: '2',
+      category_name: '개선사항',
+      title: 'V1.0.1 달력 선택 modal 사용자 경험 개선',
+      content: '',
+      description: '소셜 다이닝 글 쓰기 작성에서 달력 선택 modal이 정확히 표기되지 않은 오류가 수정되었습니다.',
+      created_at: '2024-09-22',
+      image_url: '',
+    },
+    {
+      uuid: '3',
+      category_name: '개선사항',
+      title: 'V1.0.1 달력 선택 modal 오늘 날짜 선택 기능 추가',
+      content: '',
+      description: '달력 선택 modal에서 오늘 날짜 선택 기능이 추가되었습니다.',
+      created_at: '2024-09-22',
+      image_url: '',
+    },
+    {
+      uuid: '4',
+      category_name: '업데이트',
+      title: 'V1.1.1 PC 버전 개발',
+      content: '',
+      description: '기존 모바일 뷰에서 PC뷰로 볼 수 있게되어 PC에서도 편리하게 밥피엔스 서비스를 이용할 수 있습니다.',
+      created_at: '2024-09-30',
+      image_url: '',
+    },
+    {
+      uuid: '5',
+      category_name: '업데이트',
+      title: 'V1.2.2 마이너 업데이트 - 소셜 다이닝 수정 기능 추가',
+      content: '',
+      description: '소셜 다이닝 수정 기능이 추가되었습니다(게시글 작성자만).',
+      created_at: '2024-10-01',
+      image_url: '',
+    },
+  ];
+};
