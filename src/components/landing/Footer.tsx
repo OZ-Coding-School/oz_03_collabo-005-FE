47c50257dea7f0dfbe7121857244405effd77481
jinyeongjang <wlsdud4222@gmail.com>
2024-11-15 18:47:29 +0900

    🔥 Edit: Footer 컴포넌트 수정

--- src/components/landing/Footer.tsx
+++ src/components/landing/Footer.tsx
@@ -11,30 +11,29 @@ const Footer: React.FC = () => (
 
     {/* 구분선 추가 */}
     <div className="my-4 h-px w-full bg-gray-300" />
-    <div className="text-center font-bold text-sky-800">
-      <IoBusinessOutline className="mr-2 inline-block" />
-      사업개발
+    <div className="flex flex-col justify-center space-y-4 sm:flex-row sm:space-x-8 sm:space-y-0">
+      <div className="text-center">
+        <div className="font-bold text-sky-800">
+          <IoBusinessOutline className="mr-2 inline-block" />
+          사업개발
+        </div>
+        <p className="text-sky-800">오수경, 김명석, 전두열</p>
+      </div>
+      <div className="text-center">
+        <div className="font-bold text-sky-800">
+          <TbEditCircle className="mr-2 inline-block" />
+          프론트엔드
+        </div>
+        <p className="text-sky-800">장진영, 김보람, 김준현</p>
+      </div>
+      <div className="text-center">
+        <div className="font-bold text-sky-800">
+          <TbDatabaseSearch className="mr-2 inline-block" />
+          백엔드
+        </div>
+        <p className="text-sky-800">노성우, 신현민</p>
+      </div>
     </div>
-    <p className="text-center text-sky-800">오수경, 김명석, 전두열</p>
-
-    {/* 구분선 추가 */}
-    <div className="my-4 h-px w-full bg-gray-300" />
-
-    <div className="text-center font-bold text-sky-800">
-      <TbEditCircle className="mr-2 inline-block" />
-      프론트엔드
-    </div>
-
-    <p className="text-center text-sky-800">장진영, 김보람, 김준현</p>
-
-    {/* 구분선 추가 */}
-    <div className="my-4 h-px w-full bg-gray-300" />
-
-    <div className="text-center font-bold text-sky-800">
-      <TbDatabaseSearch className="mr-2 inline-block" />
-      벡엔드
-    </div>
-    <p className="text-center text-sky-800">노성우, 신현민</p>
   </div>
 );
 
