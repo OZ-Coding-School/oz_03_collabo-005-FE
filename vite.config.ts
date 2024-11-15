ac5720924fe7bfa59670aa4ff5a3af11212d4e46
jinyeongjang <wlsdud4222@gmail.com>
2024-11-15 20:27:37 +0900

    🚑️ Fix: vite.config.ts 파일 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import path from 'path'; // 경로 처리를 위해 import
+import * as path from 'path'; // 경로 처리를 위해 import
 import react from '@vitejs/plugin-react'; // React 플러그인을 사용하기 위해 import
 import { defineConfig } from 'vite'; // Vite 설정을 정의하기 위해 import
 
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react()], // React 플러그인을 Vite 설정에 추가
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'), // '@' 별칭을 'src' 디렉토리로 설정
+      '@': path.resolve(process.cwd(), './src'), // '@' 별칭을 'src' 디렉토리로 설정
     },
   },
   build: {
